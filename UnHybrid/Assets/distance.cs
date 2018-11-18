5cab0b6b795f25d0ab50982acc59e92f223d25f1
K mehant <kmehant@gmail.com>
2018-11-18 18:36:13 +0530

    distance is implemented

---
+++ UnHybrid/Assets/distance.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class distance : MonoBehaviour {
+
+public Transform player;
+public Text dist;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		dist.text = player.position.x.ToString("0") + "m";
+	}
+}

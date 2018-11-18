823637ab46adb24ec7dadafe218a1be7b5d283f7
K mehant <kmehant@gmail.com>
2018-11-18 10:17:49 +0530

    destroy of cloned wires

---
+++ UnHybrid/Assets/destroy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroy : MonoBehaviour {
+  
+    public float lifetime = 50f;
+
+	// Use this for initialization
+	void Update () {
+		if (lifetime>0){
+			lifetime -= Time.deltaTime;
+		}
+		if (lifetime<=0){
+        Destroy(this.gameObject);
+		}
+
+	}
+	
+	
+}
--- UnHybrid/Assets/spawner.cs
+++ UnHybrid/Assets/spawner.cs
@@ -6,7 +6,7 @@ public class spawner : MonoBehaviour {
 
 public GameObject wires;
 public Vector3 spawnvalues;
-public float time =4;
+public float time =3;
 
 
 public bool stop = false;

49aba9a335bdd2c33f520d57549121e2dc11e55b
K mehant <kmehant@gmail.com>
2018-11-18 18:52:05 +0530

    player death is added

---
+++ UnHybrid/Assets/dead.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dead : MonoBehaviour {
+
+	
+	public GameObject player;
+	public GameObject spwn;
+	void OnTriggerEnter () {
+
+		
+		Destroy(player);
+		Destroy(spwn);
+
+	}
+}

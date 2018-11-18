56d6e73e819f4bf7b7b0469faf9c4ac3480d60e5
K mehant <kmehant@gmail.com>
2018-11-18 12:15:23 +0530

    Camera shake is added

---
+++ UnHybrid/Assets/shakeTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EZCameraShake;
+
+
+public class shakeTrigger : MonoBehaviour {
+  
+	public bool entered;
+	public void OnTriggerEnter () {
+         entered =true;
+
+
+	}
+	public void OnTriggerExit(){
+		entered =false;
+		CameraShaker.Instance.ShakeOnce(3f,3f,.1f,1f);
+	}
+}

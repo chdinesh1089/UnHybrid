e9b0f356bd2c8a511cb7ae85420bb08a8c44c59a
K mehant <kmehant@gmail.com>
2018-11-20 19:46:50 +0530

    first time registration locally implemented

---
+++ UnHybrid/Assets/firsttime.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class firsttime : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject registerer;
+	public InputField name;
+	public string user;
+	public string url = "http://localhost/game/dataa.php";
+	void Start () {
+		PlayerPrefs.SetInt("register",0);
+		if (PlayerPrefs.GetInt("register") == 0){
+			registerer.SetActive(true);
+			//PlayerPrefs.SetInt("register",1);
+
+		}
+		else 
+		registerer.SetActive(false);
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void getName() {
+		 user = name.text;
+		 WWWForm data = new WWWForm();
+		 data.AddField("playernamePost",user);
+		 data.AddField("scorePost",0);
+		 PlayerPrefs.GetString("username",user);
+
+		 WWW www = new WWW (url,data);
+
+	}
+}

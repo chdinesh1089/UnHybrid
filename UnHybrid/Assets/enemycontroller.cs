2101a689ff453741cd31572d2730ea8b276ddbe5
K mehant <kmehant@gmail.com>
2018-12-19 09:57:03 +0530

    Updated the environment and game components and attemting for product delivery

---
+++ UnHybrid/Assets/enemycontroller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemycontroller : MonoBehaviour {
+
+
+
+
+public GameObject trig;
+public Rigidbody trigger;
+
+ void Update ()  {
+	 transform.Translate(-.06f,0f,0f);
+	    
+trig = GameObject.FindGameObjectWithTag("dead");
+trigger = trig.GetComponent<Rigidbody>();
+ }  
+ 
+ void OnTriggerEnter (Collider player) {
+   if (player.gameObject.tag == "Player") 
+   {
+
+   trigger.isKinematic = false;
+
+   }
+ }
+
+
+}
